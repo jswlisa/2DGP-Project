f16ad773c9245a69d3ed68f3d9b91da68f5384b4
jswlisa <jswlisa@tukorea.ac.kr>
2025-10-19 11:18:40 +0900

    main.py 변경

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 from pico2d import *
+from girl import Girl
 
-open_canvas()
+def handle_events():
+    global running
 
-image = load_image('girl.png')
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            girl.handle_event(event) # SPACE 들어오면, 소년에게 전달.
 
-frame = 0
-x = 0
-y = 300
-direction = 1
 
-while True:
-    frame = (frame + 1) % 5
-    x += direction * 5
 
-    if x > 800:
-        direction = -1
-    elif x < 0:
-        direction = 1
 
+def reset_world():
+    global world
+    global girl
+
+    world = []
+
+    girl = Girl()
+    world.append(girl)
+
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
     clear_canvas()
-    image.clip_draw(frame * 256, 0, 256, 145, x, y)
+    for o in world:
+        o.draw()
     update_canvas()
 
-    delay(0.05)
 
+running = True
+
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
 close_canvas()
