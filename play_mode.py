5a9d12c1e86d7fa5d69c4aae335418c6eac856af
jswlisa <jswlisa@tukorea.ac.kr>
2025-11-10 19:24:09 +0900

    1누르면 girl, 2누르면 boy 나오도록 수정

--- play_mode.py
+++ play_mode.py
@@ -5,23 +5,48 @@ import game_framework
 import game_world
 
 from girl import Girl
+from boy import Boy
 from waterfront import WaterFront
 from enemy import Enemy
 
 girl = None
+boy = None
+girl_state = True
+boy_state = False
 
 def handle_events():
+    global girl_state, boy_state
+
     event_list = get_events()
     for event in event_list:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_1:
+            girl_state = True
+            boy_state = False
+            if boy in game_world.world[1]:
+                game_world.remove_object(boy)
+            if girl not in game_world.world[1]:
+                game_world.add_object(girl, 1)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_2:
+            boy_state = True
+            girl_state = False
+            if girl in game_world.world[1]:
+                game_world.remove_object(girl)
+            if boy not in game_world.world[1]:
+                game_world.add_object(boy, 1)
+
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         else:
-            girl.handle_event(event)
+            if girl_state:
+                girl.handle_event(event)
+            elif boy_state:
+                boy.handle_event(event)
 
 def init():
     global girl
+    global boy
 
     waterfront = WaterFront()
     game_world.add_object(waterfront, 0)
@@ -30,7 +55,10 @@ def init():
     game_world.add_object(enemy, 1)
 
     girl = Girl()
-    game_world.add_object(girl, 1)
+    # game_world.add_object(girl, 1)
+
+    boy = Boy()
+    # game_world.add_object(boy, 1)
 
 def update():
     game_world.update()
