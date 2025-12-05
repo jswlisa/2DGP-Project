c1a67b2b865023d951a2f88f8ea67227846625b7
jswlisa <jswlisa@tukorea.ac.kr>
2025-12-06 04:14:34 +0900

    enemy가 girl을 추적하는 로직 추가

--- enemy.py
+++ enemy.py
@@ -1,7 +1,12 @@
 from pico2d import *
 from state_machine import StateMachine
+import math
+import random
+
+from behavior_tree import BehaviorTree, Action, Sequence, Condition, Selector
 
 import game_framework
+import game_world
 
 # enemy Run Speed
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
@@ -15,44 +20,61 @@ TIME_PER_ACTION = 1.3
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 10.0
 
+
 def timeout(e):
     return e[0] == 'TIMEOUT'
 
+
 def die(e):
     return e[0] == 'DIE'
+
 
 class Idle:
     def __init__(self, enemy):
         self.enemy = enemy
 
     def enter(self, e):
-        self.enemy.dir = 0
-        self.enemy.frame = 0
+        pass
 
-    def exit(self,e):
-        self.enemy.dir = 0
+    def exit(self, e):
+        pass
 
     def do(self):
         self.enemy.frame = (self.enemy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
 
+        self.enemy.is_walking = False
+
+        self.enemy.bt.run()
+
     def draw(self):
-        if self.enemy.face_dir == 1:
-            self.enemy.image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 145, self.enemy.x, self.enemy.y, 256 * 2, 145 * 2)
+
+        if self.enemy.is_walking:
+            if self.enemy.face_dir == 1:
+                self.enemy.walk_image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 145,
+                                                self.enemy.x + 80, self.enemy.y - 20, 256 * 1.5, 145 * 1.5)
+            else:
+                self.enemy.walk_image.clip_composite_draw(int(self.enemy.frame) * 256, 0, 256, 145, 0, 'h',
+                                                          self.enemy.x + 80, self.enemy.y - 20, 256 * 1.5, 145 * 1.5)
         else:
-            self.enemy.image.clip_composite_draw(int(self.enemy.frame) * 256, 0, 256, 145, 0, 'h',
-                self.enemy.x, self.enemy.y, 256 * 2, 145 * 2)
+            if self.enemy.face_dir == 1:
+                self.enemy.image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 145,
+                                           self.enemy.x, self.enemy.y, 256 * 2, 145 * 2)
+            else:
+                self.enemy.image.clip_composite_draw(int(self.enemy.frame) * 256, 0, 256, 145, 0, 'h',
+                                                     self.enemy.x, self.enemy.y, 256 * 2, 145 * 2)
+
 
 class Hit:
     def __init__(self, enemy):
         self.enemy = enemy
 
-    def enter(self,e):
+    def enter(self, e):
         self.enemy.dir = 0
-        self.enemy.hp -= 76/2
+        self.enemy.hp -= 76 / 2
         if self.enemy.hp < 0:
             self.enemy.hp = 0
 
-    def exit(self,e):
+    def exit(self, e):
         self.enemy.dir = 0
 
     def do(self):
@@ -67,7 +89,7 @@ class Hit:
 
     def draw(self):
         if self.enemy.face_dir == 1:
-            self.enemy.enmey_hit_image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 145, self.enemy.x - 70,
+            self.enemy.enmey_hit_image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 145, self.enemy.x + 70,
                                                  self.enemy.y - 40, 256 * 1.7, 145 * 1.7)
         else:
             self.enemy.enmey_hit_image.clip_composite_draw(int(self.enemy.frame) * 256, 0, 256, 145, 0, 'h',
@@ -92,7 +114,8 @@ class Die:
 
     def draw(self):
         if self.enemy.face_dir == 1:
-            self.enemy.enemy_die_image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 144, self.enemy.x - 30, self.enemy.y - 20,
+            self.enemy.enemy_die_image.clip_draw(int(self.enemy.frame) * 256, 0, 256, 144, self.enemy.x + 30,
+                                                 self.enemy.y - 20,
                                                  256 * 1.4, 144 * 1.4)
         else:
             self.enemy.enemy_die_image.clip_composite_draw(int(self.enemy.frame) * 256, 0, 256, 144, 0, 'h',
@@ -103,6 +126,7 @@ class Enemy:
     def __init__(self):
         self.x, self.y = 600, 220
         self.image = load_image('enemy.png')
+        self.walk_image = load_image('enemy_walk.png')
         self.enmey_hit_image = load_image('enemy_hit.png')
         self.enemy_hp_image = load_image('hp_sprite.png')
         self.enemy_die_image = load_image('enemy_die.png')
@@ -111,6 +135,11 @@ class Enemy:
         self.dir = 0
         self.face_dir = -1
         self.hp = 456
+
+        self.speed = RUN_SPEED_PPS
+        self.tx, self.ty = 1000, 1000
+        self.build_behavior_tree()
+        self.is_walking = False
 
         self.IDLE = Idle(self)
         self.HIT = Hit(self)
@@ -123,9 +152,9 @@ class Enemy:
                 self.DIE: {die: self.DIE}
             }
         )
+
     def update(self):
         self.state_machine.update()
-
 
     def handle_event(self, event):
         self.state_machine.handle_state_event(('INPUT', event))
@@ -154,3 +183,63 @@ class Enemy:
 
     def handle_collision(self, group, other):
         pass
+
+    # Behavior Tree
+    def get_girl(self):
+        for layer in game_world.world:
+            for obj in layer:
+                if type(obj).__name__ == 'Girl':
+                    return obj
+        return None
+
+    def is_girl_nearby(self, r):
+        girl = self.get_girl()
+        if girl is None:
+            return BehaviorTree.FAIL
+
+        distance_sq = (girl.x - self.x) ** 2 + (girl.y - self.y) ** 2
+        if distance_sq < r ** 2:
+            self.target_girl = girl
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def move_to_girl(self):
+        if not hasattr(self, 'target_girl') or self.target_girl is None:
+            return BehaviorTree.FAIL
+
+        girl = self.target_girl
+
+        self.dir = math.atan2(girl.y - self.y, girl.x - self.x)
+        self.x += self.speed * math.cos(self.dir) * game_framework.frame_time
+        self.y += self.speed * math.sin(self.dir) * game_framework.frame_time
+        self.face_dir = 1 if girl.x > self.x else -1
+
+        self.is_walking = True
+
+        return BehaviorTree.RUNNING
+
+    def wander(self):
+        angle = math.atan2(self.ty - self.y, self.tx - self.x)
+        self.x += self.speed * math.cos(angle) * game_framework.frame_time
+        self.y += self.speed * math.sin(angle) * game_framework.frame_time
+        self.face_dir = 1 if self.tx > self.x else -1
+
+        self.is_walking = True
+
+        dist_sq = (self.tx - self.x) ** 2 + (self.ty - self.y) ** 2
+        if dist_sq < 50 ** 2:
+            self.tx = random.randint(100, 1100)
+            self.ty = random.randint(100, 600)
+            return BehaviorTree.SUCCESS
+
+        return BehaviorTree.RUNNING
+
+    def build_behavior_tree(self):
+        find_girl = Condition("Girl 발견?", self.is_girl_nearby, 500)
+        chase_girl = Action("Girl 추적", self.move_to_girl)
+        chase_sequence = Sequence("추적", find_girl, chase_girl)
+
+        wander_action = Action("배회", self.wander)
+
+        self.bt = Selector("행동 선택", chase_sequence, wander_action)
